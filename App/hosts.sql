d08dc0b080398eb8517db9b68e697d5e68d79c32
veracpg <vera@MacBook-Pro-de-Vera.local>
2017-04-18 12:11:40 +0200

    Templates and Forms

--- App/hosts.sql
+++ App/hosts.sql
@@ -1,7 +1,23 @@
+-- Drop Database if exists before starting test
+
+DROP DATABASE IF EXISTS hosts;
+
+--Create Database
+CREATE DATABASE hosts;
+
+--Connect to database
+\c hosts
+
+--Create sequence
+
+CREATE SEQUENCE host_id_seq INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 NO
+CYCLE;
+
+--Create host table
 CREATE TABLE
     host
     (
-        id INTEGER DEFAULT nextval('hosts_id_seq'::regclass) NOT NULL,
+        id INTEGER DEFAULT nextval('host_id_seq'::regclass) NOT NULL,
         hostname CHARACTER VARYING(128) NOT NULL,
         host_alias CHARACTER VARYING(128),
         hostgroup CHARACTER VARYING(128),
@@ -12,8 +28,33 @@ CREATE TABLE
         os_release CHARACTER VARYING(128),
         ssh_port CHARACTER VARYING(128) DEFAULT '22',
         ssh_user CHARACTER VARYING(128) DEFAULT 'root'::CHARACTER VARYING,
-        CONSTRAINT hosts_pkey PRIMARY KEY (id),
+        active BOOLEAN DEFAULT true,
+        CONSTRAINT host_pkey PRIMARY KEY (id),
         UNIQUE (ipv4),
         UNIQUE (ipv6),
         UNIQUE (hostname)
     );
+
+-- Test host_id
+
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C33NL', null, 'blazingfast', '85.151.2.83', null,
+                    'linux', 'CentOS', '6.7', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C37NL', null, 'blazingfast', '85.151.2.78', null,
+                    'linux', 'CentOS', '6.7', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C4NL', null, 'blazingfast', '85.151.2.30', null,
+                   'linux', 'CentOS', '6.7', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C5P', null, 'blazingfast', '85.151.2.173', null,
+                  'linux', 'Ubuntu', '16.04', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C1P', null, 'blazingfast', '85.151.2.84', null,
+                  'linux', 'Ubuntu', '16.04', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C3P', null, 'blazingfast', '85.151.2.95', null,
+                  'linux', 'Ubuntu', '16.04', '22', 'root', true);
+INSERT INTO host (hostname, host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release, ssh_port, ssh_user, active)
+VALUES ('BF-C4P', null, 'blazingfast', '85.151.2.171', null,
+                  'linux', 'Ubuntu', '16.04', '22', 'root', true);
--- App/project.py
+++ App/project.py
@@ -1,3 +1,80 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+import psycopg2
+
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+app = Flask(__name__)
+
+
+def connect():
+    try:
+        conn_string = "host='localhost' dbname='hosts' user='postgres' password='secret'"
+        db = psycopg2.connect(conn_string)
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print('<error message>')
+
+@app.route('/host')
+def showHome():
+    return render_template('home.html')
+
+
+@app.route('/host/add', methods=['GET', 'POST'])
+def newHost():
+    return render_template('newhost.html')
+    if request.method == 'POST':
+        db, cursor = connect()
+        query = 'INSERT INTO host (hostname, ipv4) VALUES (%s,%s);'
+        params = (hostname,ipv4,)
+        cursor.execute(query,params)
+        if request.form ['hostname'] == 0 or request.form ['ipv4'] == 0:
+            raise ValueError("Invalid new host")
+        db.commit()
+        db.close()
+        flash('New host created succefully')
+        return redirect(url_for('showHome'))
+
+
+@app.route('/host/active', methods=['GET', 'POST', 'DELETE'])
+def showActiveHosts():
+    db, cursor = connect()
+    query ='SELECT id, hostname,host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release,ssh_port, ssh_user FROM host WHERE active = TRUE;'
+    cursor.execute(query)
+    return render_template('activehost.html', items=cursor.fetchall())
+
+
+# def deleteHost(host_id):
+#     if request.method == 'DELETE':
+#         return render_template('deletehost.html', host_id = host_id)
+#
+# def editHost(host_id):
+#     if request.method == 'POST':
+#         return render_template('edithost.html', host_id = host_id)
+#
+#
+# @app.route('/host/active/<host_id>/delete', methods=['GET', 'POST', 'DELETE'])
+# def deleteHostConfirmation(host_id):
+#     if request.method == 'DELETE':
+#         db,cursor = connect()
+#         cursor.execute('DELETE FROM hosts;')
+#         db.commit()
+#         db.close()
+#         flash('Delete Host')
+#         return redirect(url_for('showActiveHosts'))
+#     if request.method == 'POST':
+#         return redirect(url_for('showActiveHosts'))
+#
+#
+#
+@app.route('/host/inactive', methods=['GET', 'POST'])
+def showInactiveHosts():
+    db, cursor = connect()
+    inactive_hosts ='SELECT id, hostname,host_alias, hostgroup, ipv4, ipv6, os, os_type, os_release,ssh_port, ssh_user FROM host WHERE active = FAlSE;'
+    cursor.execute(inactive_hosts)
+    return render_template('inactivehost.html', items=cursor.fetchall())
+#
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
